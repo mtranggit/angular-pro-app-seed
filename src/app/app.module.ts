eaeac3a196bfd4e549f585aedf8cc21e5c59f5ae
Michael Trang <michael.trang@hotmail.com>
2018-01-30 23:53:31 +1100

    Setup firebase database and hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,3 +30,15 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBR_U8cGcmX4hYBXXrQowjKPma2tAc25X4",
+    authDomain: "fitness-app-3da67.firebaseapp.com",
+    databaseURL: "https://fitness-app-3da67.firebaseio.com",
+    projectId: "fitness-app-3da67",
+    storageBucket: "fitness-app-3da67.appspot.com",
+    messagingSenderId: "435325683141"
+  };
+*/
